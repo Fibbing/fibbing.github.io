97456f907ad7482e7f86d1800cd4fca903bd63ad
Olivier Tilmans <olivier.tilmans@uclouvain.be>
2016-05-11 18:25:21 +0200

    Added files for the ECMP demo
    Signed-off-by: Olivier Tilmans <olivier.tilmans@uclouvain.be>

---
+++ files/ecmp-demo/bbb_lowres.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:51d3634183db38489d9a3c959a4d11713e0453e9b84f62afb95615b6368fa8eb
+size 81020292
---
+++ files/ecmp-demo/sintel_lowres.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:316a9fbb0fb83fced32542b80c45bbeeb29f709175ac38694207fdcf6b5abf56
+size 120753340
